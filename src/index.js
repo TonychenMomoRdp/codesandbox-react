8e07617009d5165970258c0541cb3725e5da3833
TonychenMomoRdp <46989981+TonychenMomoRdp@users.noreply.github.com>
2019-01-24 16:54:25 +0800

    react hook
    react hook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+ReactDOM.render(<App />, document.getElementById("root"));
